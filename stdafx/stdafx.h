97fd882538528f17e15a9f50bb3f28ba9e5e6497
PauloV-Almeida <Paulo_almeida4532@hotmail.com>
2026-03-26 13:47:29 -0300

    ajuste inicial

---
+++ stdafx/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <set>
+
+using namespace std;
