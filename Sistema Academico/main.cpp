217d2ddd59e312bc539c857f28acdd060dc3940c
Heropac13 <115362908+Heropac13@users.noreply.github.com>
2026-03-26 20:50:41 -0300

    Introdução V2

--- Sistema Academico/main.cpp
+++ Sistema Academico/main.cpp
@@ -4,8 +4,16 @@ struct Pessoa{
     int dia, mes, ano, idade;
 };
 
-int Calc_Idade (struct Pessoa p, int ano){
+int Calc_Idade (struct Pessoa p, int dia, int mes, int ano){
     int idd = ano - p.ano;
+    if (p.mes > mes){
+        idd = idd - 1;
+    }
+    else if (p.mes == mes){
+        if (p.dia > dia){
+            idd = idd - 1;
+        }
+    }
     return idd;
 }
 
@@ -17,8 +25,8 @@ int main (){
     Newton.dia = 4;
     Newton.mes = 1;
     Newton.ano = 1643;
-    Einstein.idade = Calc_Idade (Einstein, 2026);
-    Newton.idade = Calc_Idade (Newton, 2026);
+    Einstein.idade = Calc_Idade (Einstein, 10, 3, 2026);
+    Newton.idade = Calc_Idade (Newton, 10, 3, 2026);
     printf ("Einstein teria/faria %d anos em 2026 \n", Einstein.idade);
     printf ("Newton teria/faria %d anos em 2026 \n", Newton.idade);
     getchar ();
