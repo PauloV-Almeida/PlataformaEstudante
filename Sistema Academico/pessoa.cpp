97c55f9ddf61d1262c7ea29ab589de9206e677cd
Heropac13 <115362908+Heropac13@users.noreply.github.com>
2026-03-26 21:52:25 -0300

    V1

---
+++ Sistema Academico/pessoa.cpp
@@ -0,0 +1,24 @@
+#include "pessoa.h"
+
+Pessoa::Pessoa (int diaNa, int mesNa, int anoNa){
+    diaP = diaNa;
+    mesP = mesNa;
+    anoP = anoNa;
+    idadeP = -1;
+}
+
+void Pessoa::Calc_Idade (int diaAT, int mesAT, int anoAT){
+    idadeP = anoAT - anoP;
+    if (mesP > mesAT){
+        idadeP --;
+    }
+    else if (mesP == mesAT){
+        if (diaP > diaAT){
+            idadeP --;
+        }
+    }
+}
+
+int Pessoa::informaIdade (){
+    return idadeP;
+}
