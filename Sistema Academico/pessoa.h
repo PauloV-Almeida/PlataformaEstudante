e28931ef87fdbec0140441288374dce1e0d0b33a
Heropac13 <115362908+Heropac13@users.noreply.github.com>
2026-03-26 21:32:30 -0300

    V1

---
+++ Sistema Academico/pessoa.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct Pessoa{
+public:
+    int diaP, mesP, anoP, idadeP;
+    Pessoa (int diaNa, int mesNa, int anoNa){
+        diaP = diaNa;
+        mesP = mesNa;
+        anoP = anoNa;
+        idadeP = -1;
+    }
+    void Calc_Idade (int diaAT, int mesAT, int anoAT){
+        idadeP = anoAT - anoP;
+        if (mesP > mesAT){
+            idadeP --;
+        }
+        else if (mesP == mesAT){
+            if (diaP > diaAT){
+                idadeP --;
+            }
+        }
+    }
+};
