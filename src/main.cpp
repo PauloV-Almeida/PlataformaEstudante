f1f795d7b972d44d8e574bdd2868955ff3517c76
PauloV-Almeida <Paulo_almeida4532@hotmail.com>
2026-03-30 09:34:07 -0300

    versão 1.1

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,17 @@
 
 int main()
 {
+	/* Criando objetos Pessoa para Albert Einstein e Isaac Newton
+	 e passando atributos como parâmetros*/
 	Pessoa Einstein(14, 3, 1879, "Albert Einstein");
 	Pessoa Newton(4, 1, 1643, "Isaac Newton");
 
+
+	//Metodo para calcular a idade
 	Einstein.CalculaIdade(30, 3, 2026);
 	Newton.CalculaIdade(30, 3, 2026);
 
+	//Metodo para imprimir a idade
 	Einstein.ImprimeIdade();
 	Newton.ImprimeIdade();
 
