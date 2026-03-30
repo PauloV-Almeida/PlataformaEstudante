ffc42642e2561d038152d28a7836e9dce8b81c2e
PauloV-Almeida <Paulo_almeida4532@hotmail.com>
2026-03-30 10:02:50 -0300

    versao 2.1

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,19 @@ int main()
 	 e passando atributos como parâmetros*/
 	Pessoa Einstein(14, 3, 1879, "Albert Einstein");
 	Pessoa Newton(4, 1, 1643, "Isaac Newton");
+	Pessoa Simao(3, 10, 1976, "Jean Simao");
 
 
 	//função para calcular a idade
 	Einstein.CalculaIdade(30, 3, 2026);
 	Newton.CalculaIdade(30, 3, 2026);
+	Simao.CalculaIdade(30, 3, 2026);
 
 	//função para imprimir a idade
 	Einstein.ImprimeIdade();
 	Newton.ImprimeIdade();
+	Simao.ImprimeIdade();
 
+	
 	return 0;
 }
